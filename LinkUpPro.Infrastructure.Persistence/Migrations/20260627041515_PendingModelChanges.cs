9919f9dd5089c810de67523eca70ade9d2215471
johanvasquezdev <20251235@itla.edu.do>
2026-06-27 00:17:12 -0400

    Switch from InMemoryDatabase to Supabase PostgreSQL and add pending migrations

---
+++ LinkUpPro.Infrastructure.Persistence/Migrations/20260627041515_PendingModelChanges.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LinkUpPro.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class PendingModelChanges : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<byte[]>(
+                name: "ConcurrencyStamp",
+                table: "BattleshipGames",
+                type: "bytea",
+                rowVersion: true,
+                nullable: false,
+                defaultValue: new byte[0]);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ConcurrencyStamp",
+                table: "BattleshipGames");
+        }
+    }
+}
